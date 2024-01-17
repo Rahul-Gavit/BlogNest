dea30c7e75dd29cbb94b7def2d21a91a99615c3a
Rahul Gavit <itsmerahulgavit@gmail.com>
2024-01-17 18:37:58 +0530

    first commit

--- client/src/hooks/useSecrets.js
+++ client/src/hooks/useSecrets.js
@@ -26,7 +26,7 @@ const useSecrets = () => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          `${import.meta.env.VITE_API_BASE_URL}api/v1/secrete/all-secrete`
+          `${import.meta.env.VITE_API_BASE_URL}/api/v1/secrete/all-secrete`
         );
         setSecrets(response.data.secret);
       } catch (error) {
